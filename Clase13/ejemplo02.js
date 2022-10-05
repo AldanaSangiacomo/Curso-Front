3bab4441f755a291bab11b35a75f0ddd2a30d7d8
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-10-05 16:59:40 -0300

    Clase13

---
+++ Clase13/ejemplo02.js
@@ -0,0 +1,7 @@
+        var nombre=`Flavia`;
+        var nombre=`Laura`;
+        var edad=`40`;
+        const apellido=`Ursino`
+
+        document.write('hola soy' + nombre + '<br/>');
+        document.write(`Hola soy ${nombre}. Mi edad es ${edad}`);//interpolación de string
