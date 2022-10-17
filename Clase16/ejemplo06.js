aa94e9f272e5ce79890759ced36ca76bd97a8f27
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-10-17 12:17:30 -0300

    Clase16

---
+++ Clase16/ejemplo06.js
@@ -0,0 +1,8 @@
+const alternar=document.getElementsByClassName('alternar') [0];
+alternar.addEventListener('click',function (e){
+    e.currentTarget.classList.toggle('activo');
+})
+//class list representa una listade estilos de clases asigandas al elemento
+//toggle cambiar una clase
+//add agrega una clase
+// remove remueve una o + clases 
