a7e8f3949dd41b51447c3b3f267885d77468a1eb
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-10-26 09:25:04 -0300

    Clase19

--- Clase19/Parallax/js/codigo.js
+++ Clase19/Parallax/js/codigo.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
 
     $window = $(window);
 
-    $('#intro').height($window.height());
+    $('#intro').height($window.height()); //adapta la imagen de la ventana
     $window.scroll(function () {
-        $('div.background').each(function (index, item) {
+        $('div.background').each(function (index, item) { //el each va porque es a c7u de los elemntos del array background
             let $scroll = $(item);
-            let yPos = -($window.scrollTop() / $scroll.data('speed'));
-            let coords = '50%' + yPos + 'px';
-            $scroll.css({ backgroundPosition: coords });
+            let yPos = -($window.scrollTop() / $scroll.data('speed')); // es para que el scroll baje 
+            let coords = '50%' + yPos + 'px'; // para que el rio me quede siempre en el medio (valor en x y valor en y)
+            $scroll.css({ backgroundPosition: coords }); // toma las coordenadas declaradas en la variable coords
         })
     })
 
 })
-
