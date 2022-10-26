91a85d060c146633328800cf77ed476d1c2404fc
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-10-26 15:40:34 -0300

    Clase16

--- Clase16/ejemplo06.js
+++ Clase16/ejemplo06.js
@@ -2,7 +2,7 @@ const alternar=document.getElementsByClassName('alternar') [0];
 alternar.addEventListener('click',function (e){
     e.currentTarget.classList.toggle('activo');
 })
-//class list representa una listade estilos de clases asigandas al elemento
-//toggle cambiar una clase
-//add agrega una clase
-// remove remueve una o + clases 
+//class list representa una lista de estilos de clases asigandas al elemento
+//toggle > cambiar una clase
+//add > agrega una clase
+// remove > remueve una o + clases 
