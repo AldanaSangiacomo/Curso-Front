900557c9386727e9ca169ae7488662c698a292be
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-10-31 12:10:30 -0300

    clase22

---
+++ clase22/src/App.js
@@ -0,0 +1,17 @@
+
+import Header from './componentes/layout/Header';
+
+
+/*import './App.css';*/
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      {/* esto es un cometario */}
+    </div>
+  );
+}
+
+export default App;
---
+++ clase22/src/componentes/layout/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+    return (
+        <header>
+            <p>hola soy el Header</p>
+            <p>Lorem ipsum dolonkdcndkcndkscnckd kcfkjfdskfjdskfj kdfjksdfjksdfj </p>
+        </header>
+    )
+}
+export default Header;
