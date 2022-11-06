d084b4c3cf122e092145e0ab7cb3edcb63cd4ad4
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-11-06 11:33:22 -0300

    clase22

--- clase22/src/pages/Ejemplo1.js
+++ clase22/src/pages/Ejemplo1.js
@@ -1,6 +1,17 @@
-const Ejemplo1= props => {
-    return(
-       <div>hola soy la página ejemplo1</div> 
+
+import { Ejemplo1Props } from "../componentes/props/Props";
+
+const nombre2 = 'Pablo'
+const Ejemplo1 = props => {
+    return (
+        <div>
+            <h1>Ejempo de propiedades</h1>
+
+            <Ejemplo1Props nombre='Flavia' />
+            {/*ejemplo de propiedad simple y la puedo reutilizar*/}
+            <Ejemplo1Props nombre={nombre2} />
+            
+        </div>
     )
 }
 export default Ejemplo1
