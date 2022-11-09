c9f4292807c79262b2b5fd265792b1f4da7dcd86
Aldana Sangiacomo <asangiacomo@bld.com.ar>
2022-11-09 12:07:38 -0300

    clase22

--- clase22/src/pages/Ejemplo4.js
+++ clase22/src/pages/Ejemplo4.js
@@ -1,8 +1,17 @@
-const Ejemplo4 = props =>{
-    return(
-        <div>
-            <h1>hola soy el Ej 4 Json</h1>
-        </div>
+import alumnos from '../data/alumnos.json'
+
+const Ejemplo4 = props => {
+    return (
+        <>
+            <h1>Ej 4 Json</h1>
+            <ul>
+                {alumnos.map(alumno => (
+                    <li>{alumno.nombre} {alumno.apellido} - edad:
+                    ({alumno.edad})</li>
+
+                ))}
+            </ul>
+        </>
     )
 }
 
